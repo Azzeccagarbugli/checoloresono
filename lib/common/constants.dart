2ed0b515d9b1750cae67a7e29955df8ef4a26e5a
Francesco Coppola <f.coppola1998@gmail.com>
2021-01-09 23:16:46 +0100

    Added main logic

---
+++ lib/common/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+// Space
+const double kSpaceXS = 4.0;
+const double kSpaceS = 8.0;
+const double kSpaceM = 16.0;
+const double kSpaceL = 32.0;
+
+// Max wid
+const double kMaxWid = 512.0;
+
+// Border Radius
+BorderRadius kBorderRadius = BorderRadius.circular(12);
+
+// Duration
+const Duration kDuration = Duration(milliseconds: 600);
+
+// Curve
+const Curve kCurve = Curves.easeInToLinear;
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:checoloresono/common/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,6 @@ class MyApp extends StatelessWidget {
       title: 'Che colore sono?',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
         fontFamily: 'Computer Modern',
       ),
       home: HomePage(),
@@ -25,104 +25,191 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final PageController _controller = PageController();
+
+  final List<String> _regions = [
+    'Abruzzo',
+    'Basilicata',
+    'Calabria',
+    'Campania',
+    'Emilia Romagna',
+    'Friuli Venezia Giulia',
+    'Lazio',
+    'Liguria',
+    'Lombardia',
+    'Marche',
+    'Molise',
+    'Piemonte',
+    'Puglia',
+    'Sardegna',
+    'Sicilia',
+    'Toscana',
+    'Trentino Alto Adige',
+    'Umbria',
+    'Valle d\'Aosta',
+    'Veneto',
+  ];
+
+  int _selectedRegion = -1;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Container(
-          margin: const EdgeInsets.all(16),
-          constraints: BoxConstraints(maxWidth: 512),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Spacer(),
-              Text(
-                'Che colore sono?',
+          child: PageView(
+            physics: NeverScrollableScrollPhysics(),
+            controller: _controller,
+            children: [
+              FirstPage(
+                controller: _controller,
+              ),
+              Center(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: kSpaceS),
+                  constraints: BoxConstraints(maxWidth: kMaxWid),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Positioned.fill(
+                        child: ListView.builder(
+                          itemBuilder: (_, index) => RadioListTile(
+                            value: index,
+                            groupValue: _selectedRegion,
+                            onChanged: (region) {
+                              setState(() {
+                                _selectedRegion = region;
+                              });
+                            },
+                            activeColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: kBorderRadius,
+                            ),
+                            title: Text(
+                              _regions[index],
+                              style: TextStyle(
+                                fontFamily: 'Plex',
+                              ),
+                            ),
+                          ),
+                          itemCount: _regions.length,
+                        ),
+                      ),
+                      Positioned(
+                        left: 0,
+                        bottom: kSpaceM,
+                        right: 0,
+                        child: BlackButton(
+                          onPressed: () {},
+                          text: 'Bella zio',
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({
+    Key key,
+    this.controller,
+  }) : super(key: key);
+
+  final PageController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: kSpaceS),
+        constraints: BoxConstraints(maxWidth: kMaxWid),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Spacer(),
+            Text(
+              'Che colore sono?',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+                color: Colors.grey[800],
+              ),
+            ),
+            SizedBox(
+              height: kSpaceXS,
+            ),
+            RichText(
+              text: TextSpan(
+                text: 'Attraverso la seguente piattaforma potrai ',
                 style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                  color: Colors.grey[800],
+                  fontFamily: 'Computer Modern',
+                  fontWeight: FontWeight.w200,
+                  fontSize: 16,
+                  height: 1.4,
+                  color: Colors.grey[700],
                 ),
-              ),
-              SizedBox(
-                height: 4,
-              ),
-              RichText(
-                text: TextSpan(
-                  text: 'Attraverso la seguente piattaforma potrai ',
-                  style: TextStyle(
-                    fontFamily: 'Computer Modern',
-                    fontWeight: FontWeight.w200,
-                    fontSize: 16,
-                    color: Colors.grey[700],
-                  ),
-                  children: [
-                    TextSpan(
-                      text:
-                          'selezionare la tua regione per scoprire immediatemente a quale zona appartiene, e di conseguenza, quali sono le indicazioni che sei ',
-                      style: TextStyle(
-                        fontFamily: 'Computer Modern',
-                        fontWeight: FontWeight.w200,
-                        fontSize: 16,
-                        color: Colors.grey[700],
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'tenuto ',
-                      style: TextStyle(
-                        fontFamily: 'Computer Modern',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        letterSpacing: 0.8,
-                        color: Colors.grey[700],
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'a rispettare',
-                      style: TextStyle(
-                        fontFamily: 'Computer Modern',
-                        fontWeight: FontWeight.w200,
-                        fontSize: 16,
-                        color: Colors.grey[700],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              FlatButton.icon(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)),
-                color: Colors.black,
-                icon: Icon(
-                  Icons.flag_rounded,
-                  color: Colors.white,
-                ),
-                label: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text(
-                    'Seleziona la regione'.toUpperCase(),
+                children: [
+                  TextSpan(
+                    text:
+                        'selezionare la tua regione per scoprire immediatemente a quale zona appartiene, e di conseguenza, quali sono le indicazioni che sei ',
                     style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 0.9,
-                      fontFamily: 'Plex',
+                      fontFamily: 'Computer Modern',
+                      fontWeight: FontWeight.w200,
+                      color: Colors.grey[700],
                     ),
                   ),
-                ),
-                onPressed: () {},
+                  TextSpan(
+                    text: 'tenuto ',
+                    style: TextStyle(
+                      fontFamily: 'Computer Modern',
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 0.8,
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'a rispettare',
+                    style: TextStyle(
+                      fontFamily: 'Computer Modern',
+                      fontWeight: FontWeight.w200,
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                ],
               ),
-              Spacer(),
-              RichText(
+            ),
+            SizedBox(
+              height: kSpaceM,
+            ),
+            BlackButton(
+              text: 'Seleziona la tua regione',
+              onPressed: () {
+                controller.nextPage(duration: kDuration, curve: kCurve);
+              },
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(kSpaceM),
+              child: RichText(
                 textAlign: TextAlign.center,
                 text: TextSpan(
                   text: 'Made with love by ',
                   style: TextStyle(
                     fontFamily: 'Plex',
-                    fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.w400,
                     color: Colors.grey[400].withOpacity(0.4),
                   ),
                   children: [
@@ -137,11 +224,48 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
               ),
-            ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BlackButton extends StatelessWidget {
+  const BlackButton({
+    Key key,
+    @required this.onPressed,
+    this.text,
+  }) : super(key: key);
+
+  final String text;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton.icon(
+      shape: RoundedRectangleBorder(
+        borderRadius: kBorderRadius,
+      ),
+      color: Colors.black,
+      icon: Icon(
+        Icons.flag_rounded,
+        color: Colors.white,
+      ),
+      label: Padding(
+        padding: const EdgeInsets.all(kSpaceM),
+        child: Text(
+          text.toUpperCase(),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 0.9,
+            fontFamily: 'Plex',
           ),
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
+      onPressed: onPressed,
     );
   }
 }
